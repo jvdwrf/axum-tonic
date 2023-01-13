8abf2ed623ef26196a95827fb41a7abf241adf73
jvdwrf <jaspervdwerf@gmail.com>
2023-01-13 13:33:34 +0100

    comment out build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .out_dir("tests/common")
-        .compile(&["./tests/test.proto"], &["./"])?;
+    // tonic_build::configure()
+    //     .build_server(true)
+    //     .build_client(true)
+    //     .out_dir("tests/common")
+    //     .compile(&["./tests/test.proto"], &["./"])?;
     Ok(())
 }
