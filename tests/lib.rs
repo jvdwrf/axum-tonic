0dbe21743ef06ce8ad032d367ef9b944888a9b07
David Pasirstein <6080874+dpasirst@users.noreply.github.com>
2025-06-06 16:38:28 -0600

    feat: update tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,6 @@
 pub mod common;
 
-use std::{sync::Mutex, time::Duration};
+use std::{net::SocketAddr, sync::Mutex, time::Duration};
 
 use axum::{
     Router,
@@ -19,6 +19,8 @@ use common::{
 };
 use tokio::net::TcpListener;
 use tonic::transport::Channel;
+
+use crate::common::server::{Test1ServiceWithConnectInfo, Test2ServiceWithConnectInfo};
 
 async fn do_nothing(req: Request, next: Next) -> Result<Response, GrpcStatus> {
     Ok(next.run(req).await)
@@ -51,6 +53,59 @@ async fn main() {
         let rest_router = Router::new().merge(Router::new().route("/123", get(|| async move {})));
 
         let service = RestGrpcService::new(rest_router, grpc_router).into_make_service();
+
+        axum::serve(listener, service).await.unwrap();
+    });
+
+    tokio::time::sleep(Duration::from_millis(100)).await;
+
+    let channel = Channel::from_static(address).connect().await.unwrap();
+
+    let mut client1 = Test1Client::new(channel.clone());
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+
+    let channel = Channel::from_static(address).connect().await.unwrap();
+
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+
+    let mut client2 = Test2Client::new(channel);
+    assert_eq!(
+        client2.test2(Test2Request {}).await.unwrap_err().code(),
+        tonic::Code::Cancelled,
+    );
+}
+
+#[tokio::test]
+async fn main_connect_info() {
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let port = listener.local_addr().unwrap().port();
+    let address = Box::leak(Box::new(format!("http://127.0.0.1:{port}")));
+
+    tokio::task::spawn(async move {
+        let grpc_router1 = Router::new()
+            .nest_tonic(Test1Server::new(Test1ServiceWithConnectInfo {
+                state: Mutex::new(10),
+                str: String::new(),
+            }))
+            .layer(from_fn(do_nothing));
+
+        let grpc_router2 = Router::new()
+            .nest_tonic(Test2Server::new(Test2ServiceWithConnectInfo))
+            .layer(from_fn(cancel_request));
+
+        let grpc_router = grpc_router1.merge(grpc_router2);
+
+        let rest_router = Router::new().merge(Router::new().route("/123", get(|| async move {})));
+
+        let service = RestGrpcService::new(rest_router, grpc_router).into_make_service_with_connect_info::<SocketAddr>();
 
         axum::serve(listener, service).await.unwrap();
     });
